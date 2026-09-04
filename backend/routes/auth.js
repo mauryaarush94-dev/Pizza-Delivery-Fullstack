15d99a9210c65380413d9a06ce42d8967b173ac3
Arush Maurya <mauryaarush94@gmail.com>
2026-09-04 15:36:54 +0530

    Fire User model import

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const router = express.Router();
 
