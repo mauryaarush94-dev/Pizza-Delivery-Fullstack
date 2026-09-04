64ee6ef219bd188ade1d5eff8d8508bc93730a54
Arush Maurya <mauryaarush94@gmail.com>
2026-09-05 00:14:21 +0530

    Fix order model import

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const Order = require("../models/Order");
+const Order = require("../models/order");
 
 const router = express.Router();
 
